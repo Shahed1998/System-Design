93214c9efc69d94a032bf99ad46dbec4e041bc5e
Shahed Chowdhury Omi <shahedchowdhury120@gmail.com>
2026-06-01 17:03:16 +0100

    implemented basic events and delegates using Calculator

---
+++ Events-And-Delegates/Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+
+    delegate int MathOp(int x, int y);
+
+    static int Add(int x, int y) => x + y;
+    static int Sub(int x, int y) => x - y;
+    static int Mul(int x, int y) => x * y;
+    static int Div(int x, int y) => x / y;
+
+    public static void Main()
+    {
+        MathOp op;
+
+        op = Add;
+        Console.WriteLine("Addition of 2 and 3 =" + op(2,3));
+        
+        op = Sub;
+        Console.WriteLine("Subtration of 2 and 3 =" + op(2,3));
+        
+        op = Mul;
+        Console.WriteLine("Multiplication of 2 and 3 =" + op(2,3));
+        
+        op = Div;
+        Console.WriteLine("Division of 2 and 3 =" + op(2,3));
+    }
+}
