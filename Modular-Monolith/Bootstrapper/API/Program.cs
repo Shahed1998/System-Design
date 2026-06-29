35dc67025d5133acacfebb3503f730bc93c310cf
Shahed1998 <shahedchowdhury120@gmail.com>
2026-06-29 23:51:32 +0600

    added catalog, basket, and order module

--- Modular-Monolith/Bootstrapper/API/Program.cs
+++ Modular-Monolith/Bootstrapper/API/Program.cs
@@ -1,9 +1,21 @@
+using Basket;
+using Catalog;
+using Order;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service to the container
+builder.Services
+    .AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddOrderModule(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure HTTP pipeline
+app
+    .UseCatalogModule()
+    .UseBasketModule()
+    .UseOrderModule();
 
 app.Run();
