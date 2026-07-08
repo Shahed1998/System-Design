0ffe87d0d9550206688a423b3bea12dca76997b1
Shahed1998 <shahedchowdhury120@gmail.com>
2026-07-08 14:24:52 +0600

    applied auto migration to catalog db

--- Modular-Monolith/Modules/Catalog/Catalog/CatalogModule.cs
+++ Modular-Monolith/Modules/Catalog/Catalog/CatalogModule.cs
@@ -14,15 +14,21 @@ namespace Catalog
 
             services.AddDbContext<CatalogDbContext>(options => options.UseNpgsql(connectionStrings));
 
-
-
-
             return services;
         }
 
         public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
         {
+            // Apply migration on application startup
+            InitialiseDatabaseAsync(app).GetAwaiter().GetResult(); 
             return app;
+        }
+
+        private static async Task InitialiseDatabaseAsync(IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+            await context.Database.MigrateAsync();
         }
     }
 }
